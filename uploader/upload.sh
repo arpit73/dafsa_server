23596a7459d5ba8cebba883c353201b90f3951c6
Arpit Bharti <rpittrer@gmail.com>
2019-04-19 17:53:17 +0530

    upload now works for jpegs

--- uploader/upload.sh
+++ uploader/upload.sh
@@ -1,14 +1,21 @@
+#!/bin/sh
+# SERVER=https://kinto.dev.mozaws.net/v1
 SERVER=http://localhost:8888/v1
 
 curl -X PUT ${SERVER}/accounts/arpit73 \
      -d '{"data": {"password": "s3cr3t"}}' \
      -H 'Content-Type:application/json'
 
-BUCKET=main-workspace # (or just ``main`` in Dev)
-COLLECTION=psl
+BUCKET=main
+COLLECTION=public-suffix-list
 RECORD=`uuidgen`
-FILEPATH=/home/arpit/Development/moz/dafsa_gen/attachments/etld_data.inc
+FILEPATH=/home/arpit/Pictures/ibm.jpg
 BASIC_AUTH=arpit73:s3cr3t
+
+
+curl -X PUT ${SERVER}/buckets/${BUCKET}/collections/${CID} \
+     -H 'Content-Type:application/json' \
+     -u ${BASIC_AUTH}
 
 
 curl -s ${SERVER}/ -u $BASIC_AUTH | jq .user
@@ -17,7 +24,3 @@ curl -X POST ${SERVER}/buckets/${BUCKET}/collections/${COLLECTION}/records/${REC
      -H 'Content-Type:multipart/form-data' \
      -F attachment=@$FILEPATH \
      -u $BASIC_AUTH
-
-
-
-
