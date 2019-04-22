fcc2af185288d9d48d36705e861f6ded37d90b90
Arpit Bharti <rpittrer@gmail.com>
2019-04-22 19:56:34 +0530

    kinto extensions

--- uploader/upload.sh
+++ uploader/upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # SERVER=https://kinto.dev.mozaws.net/v1
-SERVER=http://localhost:8888/v1
+SERVER=http://3.14.70.33:8888/v1
 
 curl -X PUT ${SERVER}/accounts/arpit73 \
      -d '{"data": {"password": "s3cr3t"}}' \
@@ -9,14 +9,17 @@ curl -X PUT ${SERVER}/accounts/arpit73 \
 BUCKET=main
 COLLECTION=public-suffix-list
 RECORD=`uuidgen`
-FILEPATH=/home/arpit/Pictures/ibm.jpg
+FILEPATH=/home/arpit/Development/moz/dafsa_gen/attachments/etld_data.inc
 BASIC_AUTH=arpit73:s3cr3t
 
+
+curl -X PUT ${SERVER}/buckets/${BUCKET} \
+     -H 'Content-Type:application/json' \
+     -u ${BASIC_AUTH}
 
 curl -X PUT ${SERVER}/buckets/${BUCKET}/collections/${CID} \
      -H 'Content-Type:application/json' \
      -u ${BASIC_AUTH}
-
 
 curl -s ${SERVER}/ -u $BASIC_AUTH | jq .user
 
