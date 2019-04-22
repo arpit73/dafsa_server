88653e0a2088c5334502c0484d8c9647f1a2979e
Arpit Bharti <rpittrer@gmail.com>
2019-04-23 02:12:46 +0530

    bucket and secrets.py

--- uploader/client_setup.py
+++ uploader/client_setup.py
@@ -1,14 +1,20 @@
-#client creation with python
-
 from kinto_http import Client
+from kinto_http.patch_type import BasicPatch, MergePatch, JSONPatch
 
-#TODO Use env. variables for auth
-client = Client(server_url="https://kinto.dev.mozaws.net/v1",
-                auth=('arpit', 'p4ssw0rd'))
+from secrets import SERVER, USERNAME, PASSWORD
 
-records = client.get_records(bucket='default', collection='todos')
-for i, record in enumerate(records):
-    record['title'] = 'Todo {}'.format(i)
+credentials = (USERNAME, PASSWORD)
 
-for record in records:
-    client.update_record(record)
+client = Client(server_url=SERVER,
+                auth=credentials)
+
+# To create a bucket.
+client.create_bucket(id='payments')
+
+buckets = client.get_buckets()
+
+# To create or replace an existing bucket.
+client.update_bucket(id='payments', data={'description': 'My payments data.'})
+
+client.create_record(data={'status': 'done', 'title': 'Todo #1'},
+                     collection='receipts', bucket='payments')
